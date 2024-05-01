73c835af13d99b6f2caddcee373f2b5fe41536f6
deemoji <diman.martyanov@yandex.ru>
2024-05-01 15:45:47 +0300

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Дмитрий Мартьянов on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
